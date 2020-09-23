7a74d716584d417e1228bfe376de2c1145da3163
Ian Serrano <yenienserrano75@gmail.com>
2020-09-23 00:08:42 -0300

    Angular Seccion 3 - Diseño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NotpagesfoundComponent } from './pages/notpagesfound/notpagesfound.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    NotpagesfoundComponent,
+    BreadcrumbsComponent,
+    HeaderComponent,
+    SidebarComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/notpagesfound/notpagesfound.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notpagesfound',
+  templateUrl: './notpagesfound.component.html',
+  styleUrls: [ './notpagesfound.component.css' ]
+})
+export class NotpagesfoundComponent {
+
+  year = new Date().getFullYear()
+
+}
